65f152a541e0ff051e851bca590179745e4cd8b5
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-06 18:52:53 +0500

    Styling the widgets

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,40 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 15.0,),
+                      decoration: BoxDecoration(border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(tx.amount.toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                      ),
+                      ),
+                        ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(tx.title,
+                        style: TextStyle(fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      ),
+                        ),
+                        Text(tx.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey
+                      ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               );
             }).toList(),
           ),
