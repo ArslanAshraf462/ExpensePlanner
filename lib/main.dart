8bd614ab713c1ae8759a52c9773537f1b401a6eb
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-07 10:30:25 +0500

    String interpolation & Install packages & format date

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -64,7 +65,8 @@ class MyHomePage extends StatelessWidget {
                       ),
                       ),
                       padding: EdgeInsets.all(10),
-                      child: Text(tx.amount.toString(),
+                      child: Text(
+                        '\$${tx.amount}',
                       style: TextStyle(fontWeight: FontWeight.bold,
                       fontSize: 20,
                       color: Colors.purple,
@@ -79,7 +81,8 @@ class MyHomePage extends StatelessWidget {
                       fontSize: 18,
                       ),
                         ),
-                        Text(tx.date.toString(),
+                        Text(
+                          DateFormat.yMMMd().format(tx.date),
                         style: TextStyle(
                           color: Colors.grey
                       ),
