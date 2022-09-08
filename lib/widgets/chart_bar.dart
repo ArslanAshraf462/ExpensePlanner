7ff3f2f51eecad6a93a51a9e7e173db34e94d756
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-08 15:35:53 +0500

    Calculating size dynamically & using layout builder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChartBar extends StatelessWidget {
   final String label;
@@ -8,44 +9,55 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 10.0,
-          child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
-          ),
-        ),
-        SizedBox(height: 4.0,),
-        Container(
-          height: 60.0,
-          width: 10.0,
-          child: Stack(
-            children: [
+    return LayoutBuilder(
+      builder: (context,Constraints) {
+        return Column(
+          children: [
             Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
+              height: Constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+              ),
+            ),
+            SizedBox(
+              height: Constraints.maxHeight * 0.05,
+              ),
+            Container(
+              height: Constraints.maxHeight * 0.6,
+              width: 10.0,
+              child: Stack(
+                children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
-                color: Color.fromRGBO(220, 220, 220, 1),
-                borderRadius: BorderRadius.circular(10.0),
+                FractionallySizedBox(heightFactor: spendingPcgOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                ),
+                ],
               ),
             ),
-            FractionallySizedBox(heightFactor: spendingPcgOfTotal,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(10.0),
+            SizedBox(height: Constraints.maxHeight * 0.05,),
+            Container(
+              height: Constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(label),
+                ),
               ),
-            ),
-            ),
-            ],
-          ),
-        ),
-        SizedBox(height: 4.0,),
-        Text(label),
-      ],
+          ],
+        );
+      }
     );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,50 +9,47 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      height: 500.0,
-      child: transactions.isEmpty ? Column(
-        children: [
-          Text('No transactions added yet!',
-          style: Theme.of(context).textTheme.titleMedium,
+    return transactions.isEmpty ? Column(
+      children: [
+        Text('No transactions added yet!',
+        style: Theme.of(context).textTheme.titleMedium,
+        ),
+        SizedBox(height: 10.0,),
+        Container(
+          height: 200.0,
+          child: Image.asset('assets/images/waiting.png',fit: BoxFit.cover,),),
+      ],
+    ) 
+    : ListView.builder(
+      itemCount: transactions.length,
+      itemBuilder: ((context, index) {
+        return Card(
+          elevation: 5.0,
+          margin: EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 5.0,
           ),
-          SizedBox(height: 10.0,),
-          Container(
-            height: 200.0,
-            child: Image.asset('assets/images/waiting.png',fit: BoxFit.cover,),),
-        ],
-      ) 
-      : ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: ((context, index) {
-          return Card(
-            elevation: 5.0,
-            margin: EdgeInsets.symmetric(
-              vertical: 8.0,
-              horizontal: 5.0,
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30.0,
-                child: Padding(
-                  padding: EdgeInsets.all(6.0),
-                  child: FittedBox(
-                    child: Text('\$${transactions[index].amount}'),
-                    ),
-                ),
-              ),
-              title: Text('${transactions[index].title}',
-              style: Theme.of(context).textTheme.titleMedium,
-              ),
-              subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
-              trailing: IconButton(onPressed: () => deleteTransaction(transactions[index].id), 
-              icon: Icon(Icons.delete),
-              color: Theme.of(context).errorColor,
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30.0,
+              child: Padding(
+                padding: EdgeInsets.all(6.0),
+                child: FittedBox(
+                  child: Text('\$${transactions[index].amount}'),
+                  ),
               ),
             ),
+            title: Text('${transactions[index].title}',
+            style: Theme.of(context).textTheme.titleMedium,
+            ),
+            subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
+            trailing: IconButton(onPressed: () => deleteTransaction(transactions[index].id), 
+            icon: Icon(Icons.delete),
+            color: Theme.of(context).errorColor,
+            ),
+          ),
+        );
+      }),
           );
-        }),
-            ),
-    );
   }
 }
