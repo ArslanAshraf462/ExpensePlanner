609e5298ea01d5d759757a081814d51aa9fd5306
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-08 16:22:43 +0500

    Showing Different content on device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -114,6 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
   
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
         title: Text('Personal Expenses',style: TextStyle(fontFamily: 'Open Sans'),),
         actions: [
@@ -122,6 +123,13 @@ class _MyHomePageState extends State<MyHomePage> {
           icon: Icon(Icons.add),),
         ],
       );
+      final txListWidget = Container(
+            height: (MediaQuery.of(context).size.height - 
+              appBar.preferredSize.height- 
+              MediaQuery.of(context).padding.top)*
+              0.7,
+            child: TransactionList(_userTransactions,_deleteTransaction),
+            );
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
@@ -129,7 +137,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
+           if(isLandscape) Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text('Show Chart'),
@@ -143,20 +151,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-           _showChart ? Container(
+            if(!isLandscape)
+            Container(
+              height: (MediaQuery.of(context).size.height - 
+              appBar.preferredSize.height - 
+              MediaQuery.of(context).padding.top)*
+              0.3,
+              child: Chart(_recentTransactions),
+              ),
+              if(!isLandscape) txListWidget,
+           if(isLandscape)_showChart ? Container(
               height: (MediaQuery.of(context).size.height - 
               appBar.preferredSize.height - 
               MediaQuery.of(context).padding.top)*
               0.7,
               child: Chart(_recentTransactions),
               ):
-           Container(
-            height: (MediaQuery.of(context).size.height - 
-              appBar.preferredSize.height- 
-              MediaQuery.of(context).padding.top)*
-              0.7,
-            child: TransactionList(_userTransactions,_deleteTransaction),
-            ),
+           txListWidget,
           ],
         ),
       ),
