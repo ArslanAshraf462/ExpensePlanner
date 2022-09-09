78f1d9c4c2dad4092ee5711657d2a2918123a814
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-09 13:02:53 +0500

    Extracting Widgets & Using Builder Method

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_planner/models/transaction.dart';
+import 'package:expense_planner/widgets/transaction_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
@@ -30,41 +31,7 @@ class TransactionList extends StatelessWidget {
     : ListView.builder(
       itemCount: transactions.length,
       itemBuilder: ((context, index) {
-        return Card(
-          elevation: 5.0,
-          margin: EdgeInsets.symmetric(
-            vertical: 8.0,
-            horizontal: 5.0,
-          ),
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 30.0,
-              child: Padding(
-                padding: EdgeInsets.all(6.0),
-                child: FittedBox(
-                  child: Text('\$${transactions[index].amount}'),
-                  ),
-              ),
-            ),
-            title: Text('${transactions[index].title}',
-            style: Theme.of(context).textTheme.titleMedium,
-            ),
-            subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
-            trailing: MediaQuery.of(context).size.width>460 ? TextButton.icon(
-              style: ButtonStyle(
-                              foregroundColor: MaterialStateProperty.all(Theme.of(context).errorColor),
-                            ),
-              onPressed: () => deleteTransaction(transactions[index].id), 
-              icon: Icon(Icons.delete), 
-              label: Text('Delete'),
-              ) : 
-              IconButton(
-              onPressed: () => deleteTransaction(transactions[index].id), 
-            icon: Icon(Icons.delete),
-            color: Theme.of(context).errorColor,
-            ),
-          ),
-        );
+        return TransactionItem(transaction: transactions[index], deleteTransaction: deleteTransaction);
       }),
           );
   }
